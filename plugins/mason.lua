6996b0fe2e504a776371352fb4d2be69cc09b33e
NotZarif <flacko.programming@gmail.com>
2023-12-30 22:12:19 +0800

    'update

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -6,9 +6,6 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
-      })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
